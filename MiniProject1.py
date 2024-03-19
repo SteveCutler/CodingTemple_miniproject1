7a70f0ac08df082314a485a06f6ed91ba8b598f5
SteveCutler <stephen.c.cutler@gmail.com>
2024-03-19 17:24:50 -0400

    final commit

--- MiniProject1.py
+++ MiniProject1.py
@@ -99,25 +99,35 @@
 
 
 
-## Global variable and text color setup:
+## Global variable setup:
 
 
 toDoList = []
 dueList = []
 priorityList = []
 completionList = []
-# ColorList = []
 
 ColorReset = '\033[m' # reset to the defaults
 ColorBlue = '\033[34m' # Green Text
 ColorGreen =  '\033[32m' # Green Text
-# print (TGREEN + "Das ist es!" , ENDC)
-# print (TBLUE + "Das ist es!" , ENDC)
 
-print("Welcome to your To-Do List App!")
 
-def listDisplay():
+print("Welcome to your To-Do List App! :)")
+
+#Function Setup
+
+#Create clamp for task priority
+def clamp(n, min, max):
+    if n < min :
+        return str(min)
+    if n > max:
+        return str(max)
+    else:
+        return str(n)
     
+##Function to display To-Do list
+
+def listDisplay():   
     if bool(toDoList) != False:
         for i, element in enumerate(toDoList):
             print("name:", element, " --- ", "due:", dueList[i], " --- ", "priority:", priorityList[i], " --- ", "status:", completionList[i])
@@ -125,11 +135,13 @@ def listDisplay():
         print("There's Nothing here! Your To-Do list is empty :)")
     print("\n")
 
+##For resetting local variables in add task features
 def resetTaskVariables():
     taskTitle=""
     taskDue=""
     taskPriority=""
 
+##For changing text colour upon task completion
 def changeColor(index):
     global toDoList
     global dueList
@@ -142,7 +154,7 @@ def changeColor(index):
     completionList[index] = completionList[index].replace(ColorBlue, ColorGreen)
     print("Text Color Changed...")
     
-
+#Add task function
 def addTask(task, dueDate="n/a", priority="n/a"):
     global toDoList
     global dueList
@@ -156,32 +168,39 @@ def addTask(task, dueDate="n/a", priority="n/a"):
 
     listDisplay()
     
+#Display To-Do list function
 def viewTasks():
     print("\nOk let's pull up your To-Do List:\n")
     listDisplay()
     print("\n")
 
-
+#Function to mark task as complete -- within which we call the changeColor function
 def markAsComplete(task):
     global completionList
     #What's the name of the Task you want to change to complete?
     x = 0
+    foundItem = False
     
     while x < len(toDoList):
         if task in toDoList[x].lower():
             taskIndex = x
+            foundItem = True
             break
         else:
-            print("Couldn't find, added 1 to x")
             x +=1
+    
+    if foundItem != False:
+        changeColor(taskIndex)
+        completionList[taskIndex] = completionList[taskIndex].replace("Incomplete", "Complete")
 
-    changeColor(taskIndex)
-    completionList[taskIndex] = completionList[taskIndex].replace("Incomplete", "Complete")
+        print(f"\nYou've succesfully changed task {toDoList[x]} from Incomplete to Complete!\n")
+        listDisplay()
+        print("\n")
+    else:
+        print(f"\nCouldn't find a task named {task}. Make sure you enter a real task name!\n")
+            
 
-    print(f"\nYou've succesfully changed task {toDoList[x]} from Incomplete to Complete!\n")
-    listDisplay()
-    print("\n")
-
+#Function for deleting a task
 def delTask(task):
     global toDoList
     global dueList
@@ -191,38 +210,39 @@ def delTask(task):
     x = 0
     taskLower = task.lower()
     
+    foundItem = False
+    
     while x < len(toDoList):
         if taskLower in toDoList[x].lower():
             taskIndex = x
+            foundItem = True
             break
         else:
-            print("Couldn't find, added 1 to x")
             x +=1
+
+    if foundItem != False:
+        toDoList.pop(taskIndex)
+        dueList.pop(taskIndex)
+        priorityList.pop(taskIndex)
+        completionList.pop(taskIndex)
+
+        print(f"\nOk, the task '{task}' was succesfully removed from the list!")
+        print("Your To-Do list now looks like this:\n")
+        listDisplay()
+        print("\n")
     else:
-        print(f"Task '{task}' not found on To-Do list. Are you sure you entered the right name?")
+        print(f"\nTask '{task}' not found on To-Do list. Are you sure you entered the right name?\n")
 
-    toDoList.pop(taskIndex)
-    dueList.pop(taskIndex)
-    priorityList.pop(taskIndex)
-    completionList.pop(taskIndex)
-
-    print(f"\nOk, the task '{task}' was succesfully removed from the list!")
-    print("Your To-Do list now looks like this:\n")
-    listDisplay()
-    print("\n")
-    
-    
-
-
+##Main program while loop
 while True:
     print("Please make a selection from the following menu: (Feel free to use the item number or name, or both!)")
     print(" 1. Add a task\n 2. View tasks\n 3. Mark a task as complete\n 4. Delete a task\n 5. Quit")
 
     try:
-        userSelection = input("Which item would you like to select?\n").strip()
+        userSelection = input("Which list item would you like to select?\n").strip()
         #check to make sure only one digit in selection
 
-    except ValueError:
+    except:
         print("\nPlease make sure you enter a valid selection\n")
 
     else:
@@ -232,16 +252,38 @@ while True:
             print("Ok, lets add a new task!\n")
             
             #task creation variable input
-            taskTitle = input("Please enter a title for your new task:\n").strip()
-                #check task title for errors
-            taskDue = input(f"Please enter a due date for {taskTitle}. (If no due date desired please leave empty)\n").strip() or False
-                #check task due for errors
-            taskPriority = input(f"Please enter a priority number for the task between 1 and 5 (1 being lowest priority in 5 being highest.) \nIf no priority number desired please leave empty.\n").strip() or False
-                #check task priority for errors
+            #check task title for errors    
+            try:
+                taskTitle = input("Please enter a title for your new task:\n").strip() or "Incomplete"
+            except ValueError:
+                print("Make sure you put in an actual answer!")
+            except OverflowError:
+                print("Make sure you don't overwhelm the system!")
+                
+            #check task due for errors
+            try:
+                taskDue = input(f"Please enter a due date for {taskTitle}. (If no due date desired please leave empty)\n").strip() or False
+            except OverflowError:
+                print("Make sure you don't overwhelm the system!")
+            
+
+            #check task priority for errors
+            try:
+                taskPriority = int(input(f"Please enter a priority number for the task between 0 and 5 (0 being lowest priority in 5 being highest.) \nIf no priority number desired please leave empty.\n").strip() or 0)
+            except ZeroDivisionError:
+                print("Don't divide by zero!")
+            except ValueError:
+                print("Make sure you enter a valid Number between 0 and 5 - or leave blank!")
+            else:
+                #clamping the priority integer between 0 and 5
+                if bool(taskPriority) != False:
+                    taskPriority = clamp(taskPriority,0,5)
+                else:
+                    taskPriority = taskPriority
 
                 #check for empty responses, tailor print statement and function call to each case:
             if bool(taskTitle) == True and bool(taskDue) == False and bool(taskPriority) == False:
-                print(f"Great. You want to add a task called {taskTitle}.")
+                print(f"Great, you want to add a task called {taskTitle}.")
                 print("Lets add that to your to do list!\n")
                 addTask(taskTitle)
                 resetTaskVariables()
@@ -252,7 +294,6 @@ while True:
                 print("Lets add that to your to do list!\n")
                 addTask(taskTitle, taskDue)
                 resetTaskVariables()
-                print("Task added succesfully! Variables cleared for next time!\n")
 
             elif bool(taskTitle) == True and bool(taskDue) == True and bool(taskPriority) == True:
                 print(f"Great. You want to add a task called {taskTitle}, with a due date of {taskDue} and a priority number of {taskPriority}.")
@@ -269,50 +310,55 @@ while True:
                 print("Task added succesfully! Variables cleared for next time!\n")
 
             else:
+                #catch-all
                 print("\nAn error occurred!")
-              #  print(f"task title = {taskTitle}, task due = {taskDue}, task priority = {taskPriority}")
+                #reset local variables for task creation
                 resetTaskVariables()
-                print("Task variables cleared for next time!\n")
-                
-        
-            
-            #check task title for correct values
-            #check task due for correct values
-            #check task priority for correct values, use ceiling and floor to cut off the numbers. Make sure is int.
-            #check to see which fields are empty and tailor function call for each case so default values are used when desired.
-            #color orange for 'in progress' and green for 'completed'
-            #function call
-
-            #reset task creation variables
            
 
         elif userSelection.lower() == "view tasks" or userSelection.lower() == "2" or "2" in userSelection:
-            print("Ok, lets take a look at your to do list!")
-            #remember color coding
-            #function call
+            viewTasks()
 
         elif userSelection.lower() == "mark a task as complete" or userSelection.lower() == "3" or "3" in userSelection:
-            print("Great job on completing one of your tasks! Lets scratch it off the list")
-            finishedTaskLower = input("Which task did you complete?\n").lower()
-            markAsComplete(finishedTaskLower)
-            #taking input taskname as lowercase to reduce user error - remember to check for task name lowercase
-            #check finished task name for correct values using try except
-            #change color to green
-            #function call
+
+            if bool(toDoList) != False:
+                print("Great job on completing one of your tasks! Lets scratch it off the list")
+                try:
+                    finishedTaskLower = input("Which task did you complete?\n").lower()
+                except:
+                    print("Make sure you enter a real task name!")
+                else:
+                    if bool(finishedTaskLower) != False:
+                        markAsComplete(finishedTaskLower)
+                    else:
+                        print("\nMake sure you enter a valid task name! Try again:\n")
+            else:
+                print("\nYou can't mark a task as complete when you don't have any tasks on your list! Try adding something first :)\n")
 
         elif userSelection.lower() == "delete a task" or userSelection.lower() == "4" or "4" in userSelection:
-            print("Ok, lets remove a task.")
-            removeTaskLower = input("Which task would you like to remove?\n")
-            delTask(removeTaskLower)
-            
+            if bool(toDoList) != False:
+                print("\nOk, lets remove a task.")
+                try:
+                    removeTaskLower = input("Which task would you like to remove?\n").strip()
+                except:
+                    print("Make sure you enter a real task name!")
+                else:
+                    if bool(removeTaskLower) != False:
+                        delTask(removeTaskLower)
+                    else:
+                        print("\nMake sure you enter a valid task name! Try again:\n")
+            else:
+                print("\nYou can't remove a task from an empty list! Try adding something first :)\n")
+                
             #check input for character values
             
 
         elif userSelection.lower() == "quit" or userSelection.lower() == "5" or "5" in userSelection:
             print("Thanks for stopping by, see you soon!")
             break
-            #quit program somehow
+            #quit program
         
         else:
-            print("Please make sure you select a valid list item!")
+            print("\nPlease make sure you select a valid list item!\n")
+            #default list error display
         
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,2 +1,43 @@
-Documentation:
+Documentation for To-Do app:
 
+On launching the application you will be prompted with a menu of options:
+
+ 1. Add a task
+ 2. View tasks
+ 3. Mark a task as complete
+ 4. Delete a task
+ 5. Quit
+
+It's pretty self explanatory but I'll go into detail about each of the features and how they function:
+
+        1. Add a Task:
+    Your To-Do list starts off empty, so naturally options 3 and 4 won't do anything until a task is added.
+    On selecting the Add a Task option you will be prompted with providing a task name. You can enter whatever you want, and if you leave it blank it will default to "Incomplete" as per the instructions.
+    
+    You will then be prompted for a due date. If left empty it will default to "n/a", but you can input a due date in whatever format you wish, e.g. "Dec 12", "Tomorrow", "12pm" etc.
+
+    One final prompt for adding a task will then appear asking you to enter a priority number between 0 and 5. If a number is entered outside this range the clamp() function will clamp it between this range. If a float or string are entered it will default to "0". It will also default to "0" if left blank.
+
+    The task list will then be displayed and its default status will be "Incomplete". Incomplete items will appear blue in the list.
+
+###
+
+        2.View tasks
+    This choice calls the function to display the To-Do list.
+
+###
+
+        3.Mark a task as Complete
+    This option prompts you for the task name. If nothing is entered or a task that doesn't exist it will print a message and return you to the menu.
+    If the user enters a correct task name, the corresponding task status will be changed from "Incomplete" to "Complete" and the colour of the task in the To-Do list will change from Blue to Green and it will display it like this from now on.
+
+###
+
+        4.Delete a task
+    This option prompts the user for a task name once again. Similarly to the "Mark a task as complete" option, if the prompt is left empty or a task that doesn't exist is entered, the program will print a prompt telling you to make sure you enter a real task.
+    If a task is correctly entered, the task will be removed from the ToDo list and the new list will be displayed.
+
+###
+
+        5.Quit
+    This quits the 'while' loop that effectively keeps the program running. The program is exited and the list data will not be retained.
