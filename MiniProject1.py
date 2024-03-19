e6e7f7f9473c6ff650abf6af9d5217062182f16f
SteveCutler <stephen.c.cutler@gmail.com>
2024-03-19 11:08:59 -0400

    further project structure layout

--- MiniProject1.py
+++ MiniProject1.py
@@ -124,16 +124,46 @@ try:
 except ValueError:
     print("Please make sure you enter a valid selection")
 else:
+    #check to make sure is all either digit or alpha else throw print statement
     if userSelection.lower() == "add a task" or userSelection.lower() == "1":
-        print("you selected task 1")
+
+        print("Ok, lets add a new task!")
+
+        taskTitle = input("Please enter a title for your new task:\n")
+        taskDue = input(f"Please enter a due date for {taskTitle}. (If no due date desired please leave empty)\n")
+        taskPriority = input(f"Please enter a priority number for the task between 1 and 5 (1 being lowest priority in 5 being highest.) If no priority number desired please leave empty.\n")
+        #check for empty responses, tailor print statement to each case:
+       
+        print(f"Great. You want to add a task called {taskTitle}, with a due date of {taskDue} and a priority number of {taskPriority}.")
+        print("Lets add that to your to do list!")
+        #check task title for correct values
+        #check task due for correct values
+        #check task priority for correct values, use ceiling and floor to cut off the numbers. Make sure is int.
+        #check to see which fields are empty and tailor function call for each case so default values are used when desired.
+        #color orange for 'in progress' and green for 'completed'
+        #function call
+
     elif userSelection.lower() == "view tasks" or userSelection.lower() == "2":
-        print("you selected task 2")
+        print("Ok, lets take a look at your to do list!")
+        #remember color coding
+        #function call
+
     elif userSelection.lower() == "mark a task as complete" or userSelection.lower() == "3":
-        print("you selected task 3")
+        print("Great job on completing one of your tasks! Lets scratch it off the list")
+        finishedTaskLower = input("Which task did you complete?\n").lower()
+        #taking input taskname as lowercase to reduce user error - remember to check for task name lowercase
+        #check finished task name for correct values using try except
+        #change color to green
+        #function call
     elif userSelection.lower() == "delete a task" or userSelection.lower() == "4":
-        print("you selected task 4")
+        print("Ok, lets remove a task.")
+        removeTask = input("Which task would you like to remove?").lower()
+        #converted to lowercase to reduce user error, remember to check for lowercase task on list
+        #check input for character values
+        #function call
     elif userSelection.lower() == "quit" or userSelection.lower() == "5":
-        print("you selected task 5")
+        print("Thanks for stopping by, see you soon!")
+        #quit program somehow
     else:
         print("Please make sure you select a valid list item!")
     
